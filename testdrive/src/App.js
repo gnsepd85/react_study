872b4614488889cdf4943762fc820bd7a38093e6
gnsepd85 <49679836+gnsepd85@users.noreply.github.com>
2019-05-14 20:56:54 +0900

    190514
    버튼삽입

--- testdrive/src/App.js
+++ testdrive/src/App.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
+import Main from '../src/components/main'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.<br></br>
-          앙기모찌~
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Main/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ testdrive/src/components/main/main.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class main extends Component 
+{
+    state = {click_count : 0}
+    
+
+    render() 
+    {
+        return (
+            <div>
+                <p>현재 클릭한횟수는 {this.state.click_count} 입니다.</p>
+                <button onClick={()=> this.setState({click_count: 5})}>
+                    증가
+                </button>
+                <button onClick={()=> this.setState({click_count: 0})}>
+                    감소
+                </button>
+                
+            </div>
+        );
+    }
+}
+
+export default main;
